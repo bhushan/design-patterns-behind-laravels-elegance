625618cdb6389e948d2aec081fb47837fa1faae5
Bhushan <bhushangaykawad@gmail.com>
2025-08-31 08:55:56 +0530

    WIP

--- routes/web.php
+++ routes/web.php
@@ -1,68 +1,81 @@
 <?php
 
-abstract class Handler
-{
-    private ?Handler $next = null;
+ cache()->set('meetup', 'Laravel Ahmedabad');
+ dd(
+    cache()->get('meetup')
+ );
 
-    public function setNext(Handler $handler): Handler
-    {
-        $this->next = $handler;
 
-        return $handler;
-    }
 
-    public function handle(string $request): ?string
-    {
-        if ($this->next) {
-            return $this->next->handle($request);
-        }
 
-        return null;
-    }
-}
 
-class AuthHandler extends Handler
-{
-    public function handle(string $request): ?string
-    {
-        if ($request === 'unauthorized') {
-            return 'AuthHandler: Access Denied!';
-        }
 
-        return parent::handle($request);
-    }
-}
 
-class LoggerHandler extends Handler
-{
-    public function handle(string $request): ?string
-    {
-        dump("LoggerHandler: Logging request -> $request");
 
-        return parent::handle($request);
-    }
-}
 
-class DataHandler extends Handler
-{
-    public function handle(string $request): ?string
-    {
-        if ($request === 'data') {
-            return 'DataHandler: Processing data...';
-        }
 
-        return parent::handle($request);
-    }
-}
 
-$auth = new AuthHandler;
-$logger = new LoggerHandler;
-$data = new DataHandler;
 
-$auth->setNext($logger)->setNext($data);
 
-dd(
-    $auth->handle('unauthorized'),
-    $auth->handle('data'),
-    $auth->handle('other')
-);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class Human
+//{
+//    public function walk(): string
+//    {
+//        return 'walk';
+//    }
+//
+//    public function eat(): string
+//    {
+//        return 'eat';
+//    }
+//
+//    public function sleep(): string
+//    {
+//        return 'sleep';
+//    }
+//}
+//
+//app()->bind('human', function () {
+//    return new Human;
+//});
+//
+//class HumanFacade
+//{
+//    public static function __callStatic($method, $args)
+//    {
+//        return app()->make('human')->{$method}(...$args);
+//    }
+//}
+//
+//dd(
+//    HumanFacade::sleep()
+//);
