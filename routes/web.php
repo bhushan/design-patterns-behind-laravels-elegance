8cce78dc885a938608a68d9b7dd6b0bc74a50970
Bhushan <bhushangaykawad@gmail.com>
2025-10-25 17:04:27 +0530

    WIP

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,70 @@
 <?php
 
-class Human
+interface PizzaBuilderInterface
 {
-    public function walk()
+    public function setDough(string $dough): self;
+
+    public function setSauce(string $sauce): self;
+
+    public function setTopping(string $topping): self;
+
+    public function build(): Pizza;
+}
+
+class Pizza
+{
+    public string $dough;
+
+    public string $sauce;
+
+    public string $topping;
+
+    public function __toString(): string
     {
-        dump('walk');
-
-        return $this;
-    }
-
-    public function eat()
-    {
-        dump('eat');
-
-        return $this;
-    }
-
-    public function sleep()
-    {
-        dump('sleep');
-
-        return $this;
+        return "Pizza with {$this->dough} dough, {$this->sauce} sauce, and {$this->topping} topping.";
     }
 }
 
-(new Human)->eat()->walk()->sleep();
+class PizzaBuilder implements PizzaBuilderInterface
+{
+    private Pizza $pizza;
+
+    public function __construct()
+    {
+        $this->pizza = new Pizza;
+    }
+
+    public function setDough(string $dough): self
+    {
+        $this->pizza->dough = $dough;
+
+        return $this;
+    }
+
+    public function setSauce(string $sauce): self
+    {
+        $this->pizza->sauce = $sauce;
+
+        return $this;
+    }
+
+    public function setTopping(string $topping): self
+    {
+        $this->pizza->topping = $topping;
+
+        return $this;
+    }
+
+    public function build(): Pizza
+    {
+        return $this->pizza;
+    }
+}
+
+$builder = new PizzaBuilder;
+$pizza = $builder->setDough('thin')
+    ->setSauce('tomato')
+    ->setTopping('cheese')
+    ->build();
+
+dd((string) $pizza);
